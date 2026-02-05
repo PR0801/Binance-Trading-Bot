86dc76ebb82824bc11c3afa539bf0881192ac193
Pratyush Raunak <pratyushraunak2019@gmail.com>
2026-02-06 00:27:29 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -1,1 +1,12 @@
-
+from binance.client import Client
+import os
+from dotenv import load_dotenv
+load_dotenv()
+class BinanceClient:
+    def __init__(self):
+        api_key = os.getenv("BINANCE_API_KEY")
+        api_secret = os.getenv("BINANCE_API_SECRET")
+        self.client = Client(api_key, api_secret)
+        self.client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
+    def get_client(self):
+        return self.client
