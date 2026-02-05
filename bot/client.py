5d1abcc16f8aaeb71fde410633f4ff03cbac7996
Pratyush Raunak <pratyushraunak2019@gmail.com>
2026-02-06 00:39:46 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -1,11 +1,15 @@
-from binance.client import Client
 import os
 from dotenv import load_dotenv
-load_dotenv()
+from binance.client import Client
+import streamlit as st
 class BinanceClient:
     def __init__(self):
+        load_dotenv()
         api_key = os.getenv("BINANCE_API_KEY")
         api_secret = os.getenv("BINANCE_API_SECRET")
+        if not api_key and "BINANCE_API_KEY" in st.secrets:
+            api_key = st.secrets["BINANCE_API_KEY"]
+            api_secret = st.secrets["BINANCE_API_SECRET"]
         self.client = Client(api_key, api_secret)
         self.client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
     def get_client(self):
